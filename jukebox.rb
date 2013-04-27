db22e2e147456da2d49537bcef715451c3c3b8ce
Michael F. Weinberg <michael@luckymike.com>
2013-04-27 11:51:39 -0700

    Add disk/track verification.

--- jukebox.rb
+++ jukebox.rb
@@ -1,5 +1,6 @@
 #!/usr/local/bin/ruby
 
+require "json"
 require "serialport"
 
 port_str = "/dev/cu.usbserial-A6006hSj"
@@ -10,11 +11,19 @@ parity = SerialPort::NONE
 
 sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
 
+collection = JSON.parse(File.read("/tmp/collection.txt"))
+
 while true do
   while (selection = sp.gets.chomp) do
-    disk = selection[0..1]
+    disc = selection[0..1]
     track = selection[2..3]
-    `./upnext.sh #{disk} #{track}`
+    chk_disc = disc.to_i-1
+    chk_track = track.to_i-1
+    unless collection[chk_disc].nil?
+      unless collection[chk_disc][chk_track].nil?
+        `./upnext.sh #{disc} #{track}`
+      end
+    end
   end
 end
 
