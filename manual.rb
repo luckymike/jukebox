c6a12f9a80349c0d27f11c5287123775a8efbddd
Michael F. Weinberg <michael@luckymike.com>
2013-04-27 11:50:55 -0700

    Manual entry testing script.

---
+++ manual.rb
@@ -0,0 +1,21 @@
+#!/usr/local/bin/ruby
+
+require "json"
+
+selection = (ARGV[0])
+collection = JSON.parse(File.read("/tmp/collection.txt"))
+
+disc = selection[0..1]
+track = selection[2..3]
+chk_disc = disc.to_i-1
+chk_track = track.to_i-1
+unless collection[chk_disc].nil?
+  unless collection[chk_disc][chk_track].nil?
+    puts "Thank you!"
+    `./upnext.sh #{disc} #{track}`
+  else
+    puts "Track not found."
+  end
+else
+  puts "Track not found."
+end
