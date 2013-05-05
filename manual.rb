2815fac36ea478a74c58b31d0c7fa0aa5db95bd8
Michael F. Weinberg <michael@luckymike.com>
2013-05-05 13:23:56 -0700

    Updated manual jukebox script.

--- manual.rb
+++ manual.rb
@@ -1,22 +1,52 @@
 #!/usr/bin/env ruby
 
-rquire "rubygems"
+require "rubygems"
 require "json"
 
 selection = (ARGV[0])
 collection = JSON.parse(File.read("/tmp/collection.txt"))
 
-disc = selection[0..1]
-track = selection[2..3]
-chk_disc = disc.to_i-1
-chk_track = track.to_i-1
-unless collection[chk_disc].nil?
-  unless collection[chk_disc][chk_track].nil?
-    puts "Thank you!"
-    `./upnext.sh #{disc} #{track}`
+def validate_entry(entry, against)
+  disc = entry[0..1]
+  track = entry[2..3]
+  if (disc == "00" || track == "00" || entry.include?("#") || entry.include?("*"))
+    puts "invalid selection"
+    validates = false
   else
-    puts "Track not found."
+    chk_disc = disc.to_i-1
+    chk_track = track.to_i-1
+    if against[chk_disc].nil?
+      puts "disc not found"
+      validates = false
+    else
+      if against[chk_disc][chk_track].nil?
+        puts "track not found"
+        validates = false
+      else
+        puts "added #{entry}"
+        validates = true
+        `./upnext.sh #{disc} #{track}`
+        disc = nil
+        track = nil
+      end
+    end
+  end
+  return validates
+end
+
+def itunes_status(status)
+   (`osascript -e 'tell application "iTunes" to player state as string'`).gsub("\n","")==(status)
+end
+
+def itunes_play(playlist)
+  `osascript -e 'tell application "iTunes"' -e 'set new_playlist to \"#{playlist}\" as string' -e 'play playlist new_playlist' -e 'end tell'`
+end
+
+if validate_entry(selection, collection)
+  puts "valid selection"
+  unless itunes_status("playing")
+    itunes_play("Jukebox")
   end
 else
-  puts "Track not found."
+  puts "invalid selection"
 end
